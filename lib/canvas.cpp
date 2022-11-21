896aec47a402036bc932c065381030295c9e3a73
luisesales <duduosales@hotmail.com>
2022-11-21 16:24:21 -0300

    correção do canvas

--- lib/canvas.cpp
+++ lib/canvas.cpp
@@ -9,7 +9,11 @@ namespace life {
             
 
   Canvas::Canvas(size_t w = 0, size_t h = 0, short bs = 5){
-    m_pixels.reserve(w*h*bs);
+    m_width = w;
+    m_height= h;
+    m_block_size = bs;
+
+    m_pixels.reserve(w*h*image_depth);
   }
     /// Destructor.
   Canvas::~Canvas(void){}
@@ -28,19 +32,35 @@ namespace life {
   //=== Members
   /// Clear the canvas with black color.
   void Canvas::clear(const Color& c = BLACK){
-    for(size_t i{0}; i < m_pixels.size();i+=4){
-      m_pixels[i] = c.channels[Color::R];
-      m_pixels[i+1] = c.channels[Color::G];
-      m_pixels[i+2] = c.channels[Color::B];
-      m_pixels[i+3] = 255;
+    for(size_t i{0}; i <= (m_pixels.size()-image_depth);i+=image_depth){
+        m_pixels[i] = c.channels[Color::R];
+        m_pixels[i+1] = c.channels[Color::G];
+        m_pixels[i+2] = c.channels[Color::B];
+        m_pixels[i+3] = 255;
     }
   }
   /// Set the color of a pixel on the canvas.
   void Canvas::pixel(coord_t row, coord_t col, const Color& c){
-    m_pixels[row*col*image_depth] = c.channels[Color::R];
-    m_pixels[row*col*image_depth+1] = c.channels[Color::G];
-    m_pixels[row*col*image_depth+2] = c.channels[Color::B];
-    m_pixels[row*col*image_depth+3] = 255;
+    //Check if params are greater or equal than the size of the vector
+    if(row >= m_width || col >= m_height) return;
+    
+    //Change into real pixel sizes
+    row*=m_block_size;
+    col*=m_block_size;
+    
+    //Color The Pixel with its color
+    for (size_t i{0}; i <= m_block_size; i++){
+      for(size_t j{0}; j <= m_block_size; j++){
+        auto b_offset = (row+i)*(col+j)*image_depth;
+        m_pixels[b_offset] = c.channels[Color::R];
+        m_pixels[b_offset+1] = c.channels[Color::G];
+        m_pixels[b_offset+2] = c.channels[Color::B];
+        m_pixels[b_offset+3] = 255;
+      }
+      
+    }
+    
+    
   }
   /// Get the pixel color from the canvas.
   Color Canvas::pixel(coord_t row, coord_t col) const{
